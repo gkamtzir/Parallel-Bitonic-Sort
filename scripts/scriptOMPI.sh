e4969b144411ff2949a683f00b8fd47250f300e9
nicktheway <nikospa96@hotmail.gr>
2017-11-17 22:54:54 +0200

    fixed broken path

--- scripts/scriptOMPI.sh
+++ scripts/scriptOMPI.sh
@@ -15,7 +15,7 @@ do
 		i=$MIN_ARRAY_SIZE
 		while [ $i -le $MAX_ARRAY_SIZE ]
 		do
-			../dbin/datagenOMPI "$i" "$h" "$MAX_ARRAY_SIZE"
+			../datagen_bin/datagenOMPI "$i" "$h" "$MAX_ARRAY_SIZE"
 			i=$(($i + 1))
 		done
 		if [ $j != $SAMPLES ]
